cb514ccf94116e4bd04119c639247102fe3344e9
jdreyesp <jdiego.reyes@gmail.com>
2022-01-31 20:14:11 +0100

    Windowing + Modifications

--- src/main/scala/com/jdreyesp/learningspark/chapter5/OperationsDF.scala
+++ src/main/scala/com/jdreyesp/learningspark/chapter5/OperationsDF.scala
@@ -3,7 +3,7 @@ package com.jdreyesp.learningspark.chapter5
 import com.jdreyesp.learningspark.SparkSessionInitializer
 import org.apache.spark.sql.functions.{col, expr}
 
-object UnionsJoinsWindowingDF extends App with SparkSessionInitializer {
+object OperationsDF extends App with SparkSessionInitializer {
 
   // #1. LOAD AND PREPARE DATA
 
@@ -56,5 +56,57 @@ object UnionsJoinsWindowingDF extends App with SparkSessionInitializer {
     col("air.IATA") === col("origin")
     // Inner join
   ).select("City", "State", "date", "delay", "distance", "destination").show()
+
+  // #4. WINDOWING
+  spark.sql(
+    """
+      SELECT origin, destination, SUM(delay) AS TotalDelays
+      FROM departureDelays
+      WHERE origin IN ('SEA', 'SFO', 'JKF')
+      AND destination IN ('SEA', 'SFO', 'JFK', 'DEN', 'ORD', 'LAX', 'ATL')
+      GROUP BY origin, destination
+      """).createOrReplaceTempView("departureDelaysWindow")
+
+  spark
+    .sql(
+    """
+      SELECT origin, destination, TotalDelays, rank
+      FROM (
+        SELECT origin, destination, TotalDelays, dense_rank()
+        OVER (PARTITION BY origin ORDER BY TotalDelays DESC) as rank
+        FROM departureDelaysWindow
+      ) t
+      WHERE rank <= 3
+      """).show()
+
+  // #5. MODIFICATIONS
+
+  // Adding columns
+  val foo2 = foo.withColumn("status", expr("CASE WHEN delay <= 10 THEN 'On-time' ELSE 'Delayed' END"))
+  foo2.show()
+
+  // Dropping columns
+  val foo3 = foo2.drop("delay")
+  foo3.show()
+
+  // Renaming column
+  val foo4 = foo3.withColumnRenamed("status", "flight_status")
+  foo4.show()
+
+  // Pivoting columns
+  // Pivoting allows renaming columns as well as performing aggregate calculations
+  val foo5 = spark.sql(
+    """
+      SELECT * FROM (
+      SELECT destination, CAST(SUBSTRING(date, 0, 2) AS int) AS month, delay
+      FROM departureDelays
+      WHERE origin = 'SEA')
+      PIVOT (
+        CAST(AVG(delay) AS DECIMAL(4,2)) AS AvgDelay, MAX(delay) AS MaxDelay
+        FOR month IN (1 JAN, 2 FEB)
+      )
+      ORDER BY destination
+      """).show()
+
 
 }

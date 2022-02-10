b927bf45810f8af7de705ed28918958082eb0398
reyespon <juandiego.reyesponce@tomtom.com>
2022-02-10 18:17:57 +0100

    Added deltalake exercise

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.12.15"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.2.0",
   "org.apache.spark" %% "spark-sql" % "3.2.0",
+  "io.delta" %% "delta-core" % "1.1.0",
   "org.scalatest" %% "scalatest" % "3.2.10" % Test
 )
--- src/main/scala/com/jdreyesp/learningspark/SparkSessionInitializer.scala
+++ src/main/scala/com/jdreyesp/learningspark/SparkSessionInitializer.scala
@@ -8,6 +8,8 @@ trait SparkSessionInitializer {
     .appName("Learning spark")
     .config("spark.driver.bindAddress", "127.0.0.1")
     .config("spark.driver.host", "127.0.0.1")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
     .master("local")
     .getOrCreate()
 }

45b7282505aeb7b63a047ebc99264743240ed138
jdreyesp <jdiego.reyes@gmail.com>
2022-02-19 11:40:56 +0100

    Complex type udf

--- src/main/scala/com/jdreyesp/learningspark/chapter5/UDF.scala
+++ src/main/scala/com/jdreyesp/learningspark/chapter5/UDF.scala
@@ -19,4 +19,33 @@ object UDF extends App with SparkSessionInitializer {
   // Just define the udf using `pandas_udf(...)`
   // More info: https://databricks.com/blog/2017/10/30/introducing-vectorized-udfs-for-pyspark.html
 
+  // It does not have to be a primitive data type that can be processed / returned by an UDF. It can also accept / return complex types
+  // Let's see this in an example:
+
+  case class PersonalChar(charisma: Double, bravery: Double)
+  case class PhysicalChar(strength: Double, dexterity: Double)
+  case class Human(personalChar: PersonalChar, physicalChar: PhysicalChar)
+  case class Elf(personalChar: PersonalChar, physicalChar: PhysicalChar)
+
+  val humanToElfPersonalChar = (humanPersonalChar: PersonalChar) => PersonalChar(humanPersonalChar.charisma * 2, humanPersonalChar.bravery / 2)
+  val humanToElfPhysicalChar = (humanPhysicalChar: PhysicalChar) => PhysicalChar(humanPhysicalChar.strength / 2, humanPhysicalChar.dexterity * 3)
+
+  spark.udf.register("humanToElfPersonalChar", humanToElfPersonalChar)
+  spark.udf.register("humanToElfPhysicalChar", humanToElfPhysicalChar)
+
+  import spark.implicits._
+
+  val df = Seq(Human(PersonalChar(12, 15), PhysicalChar(10, 12))).toDF
+  df.createOrReplaceTempView("fantasyPeople")
+
+  spark.sql(
+    """
+      SELECT humanToElfPersonalChar(personalChar) AS personalChar,
+             humanToElfPhysicalChar(physicalChar) AS physicalChar
+      FROM fantasyPeople""")
+    .as[Elf]
+    .show(false)
+
+
+
 }

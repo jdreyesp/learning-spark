3d4e13990b397d5c204e6624152979a401571b08
jdreyesp <jdiego.reyes@gmail.com>
2022-02-19 13:14:43 +0100

    More join doc

--- src/main/scala/com/jdreyesp/learningspark/chapter7/Joins.scala
+++ src/main/scala/com/jdreyesp/learningspark/chapter7/Joins.scala
@@ -158,5 +158,29 @@ object Joins extends App with SparkSessionInitializer {
   //                        +- FileScan parquet default.userstbl[uid#205,login#206,email#207,user_state#208] Batched: true, DataFilters: [], Format: Parquet, Location: InMemoryFileIndex(1 paths)[file:..., PartitionFilters: [], PushedFilters: [], ReadSchema: struct<uid:int,login:string,email:string,user_state:string>, SelectedBucketsCount: 8 out of 8
   joinUsersOrdersBucketDF.explain()
 
+  //For checking Spark UI
   while(true){}
+
+  /** How Spark selects join strategy:
+   If it is an ‘=’ join (equi-join):
+    Look at the join hints, in the following order:
+      1. Broadcast Hint: Pick broadcast hash join if the join type is supported.
+      2. Sort merge hint: Pick sort-merge join if join keys are sortable.
+      3. shuffle hash hint: Pick shuffle hash join if the join type is supported.
+      4. shuffle replicate NL hint: pick cartesian product if join type is inner like.
+    If there is no hint or the hints are not applicable
+      1. Pick broadcast hash join if one side is small enough to broadcast, and the join type is supported.
+      2. Pick shuffle hash join if one side is small enough to build the local hash map, and is much smaller than the other side, and spark.sql.join.preferSortMergeJoin is false.
+      3. Pick sort-merge join if join keys are sortable.
+      4. Pick cartesian product if join type is inner .
+      5. Pick broadcast nested loop join as the final solution. It may OOM but there is no other choice.
+
+  If it’s not ‘=’ join:
+    Look at the join hints, in the following order:
+      1. broadcast hint: pick broadcast nested loop join.
+      2. shuffle replicate NL hint: pick cartesian product if join type is inner like.
+    If there is no hint or the hints are not applicable
+      1. Pick broadcast nested loop join if one side is small enough to broadcast.
+      2. Pick cartesian product if join type is inner like.
+      3. Pick broadcast nested loop join as the final solution. It may OOM but we don’t have any other choice. */
 }
